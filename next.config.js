058adaf079a115631ad411570b68906020d7e0bf
marcovalentino-dev <marco.valentino1996@gmail.com>
2025-08-06 23:35:00 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    serverActions: true,
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
   },
 };
 
